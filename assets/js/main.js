94989b35a8cd982d11ec9df83914025ec9d2f1f1
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-02-10 18:26:32 +0000

    + all code for function, this is a really simple project

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+const quoteSpace = document.querySelector('#quote');
+const authorSpace = document.querySelector('#author');
+
+let req = new XMLHttpRequest();
+
+req.open('GET', 'https://type.fit/api/quotes');
+req.addEventListener('load', (e) => {
+    let quotes = JSON.parse(e.target.responseText);
+    let quote = quotes[Math.round(Math.random() * quotes.length)];
+    console.log({quotes});
+    console.log({quote});
+    
+    quoteSpace.innerHTML = owoify(quote.text);
+    authorSpace.innerHTML = quote.author;
+});
+req.send();
+
+function owoify(text) {
+    text = text.replace(/(?:r|l)/g, "w");
+    text = text.replace(/(?:R|L)/g, "W");
+    text = text.replace(/n([aeiou])/g, 'ny$1');
+    text = text.replace(/N([aeiou])/g, 'Ny$1');
+    text = text.replace(/N([AEIOU])/g, 'Ny$1');
+    text = text.replace(/ove/g, "uv");
+    return text
+}
