a97e7074c0c45691bbe3fac0b4bbdedcf7142882
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-02-11 06:44:15 +0000

    + tweet button is added dynamically

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const quoteSpace = document.querySelector('#quote');
 const authorSpace = document.querySelector('#author');
+const twitterButton = document.querySelector('#twitterButton');
 
 let req = new XMLHttpRequest();
 
@@ -7,9 +8,12 @@ req.open('GET', 'https://type.fit/api/quotes');
 req.addEventListener('load', (e) => {
     let quotes = JSON.parse(e.target.responseText);
     let quote = quotes[Math.round(Math.random() * quotes.length)];
+    let owoQuote = owoify(quote.text);
     
-    quoteSpace.innerHTML = owoify(quote.text);
+    quoteSpace.innerHTML = owoQuote;
     authorSpace.innerHTML = quote.author;
+    document.querySelector('body').innerHTML += `<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="${owoQuote} -${quote.author}" data-url="https://owo-quotes.glitch.me/" data-related="UnicornNukes" data-show-count="false" id="twitterButton">Tweet</a>`
+    twttr.widgets.load();
 });
 req.send();
 
